ccd0dcb374c8380cd68a2bfe1313b935d2a6e897
Clement Denis <le.mikmac@gmail.com>
2021-04-01 04:24:45 +0100

    test implicit redirect uri for discord

--- oauth.js
+++ oauth.js
@@ -5,33 +5,20 @@ const rand = () => Math.random().toString(36).slice(2, 12).padEnd(10, '0')
 const state = localStorage.state || `${rand()}-${rand()}`
 localStorage.state = state
 
-// Request a user's GitHub identity
-export const githubUrl = `https://github.com/login/oauth/authorize?${new URLSearchParams(
-  {
-    // Required. The client ID you received from GitHub when you registered.
-    client_id: `07957ad810a70e99d67c`,
+const oauth2Url = (url, args) => `https://${url}?${new URLSearchParams(args)}`
 
-    // The URL in your application where users will be sent after authorization.
-    // DEV && `redirect_uri=${encodeURIComponent('http://localhost:8080')}`,
+export const githubUrl = oauth2Url('github.com/login/oauth/authorize', {
+  client_id: `07957ad810a70e99d67c`,
+  state,
+  scope: 'user',
+})
 
-    // A random state
-    state,
-
-    // A space-delimited list of scopes.
-    scope: 'user',
-  },
-)}`
-
-// Request a user's GitHub identity
-export const discordUrl = `https://discordapp.com/api/oauth2/authorize?${new URLSearchParams(
-  {
-    client_id: '826974634069983282',
-    state,
-    redirect_uri: window.location.origin,
-    response_type: 'code',
-    scope: 'identify gdm.join guilds.join',
-  },
-)}`
+export const discordUrl = oauth2Url('discordapp.com/api/oauth2/authorize', {
+  client_id: '826974634069983282',
+  state,
+  response_type: 'code',
+  scope: 'identify gdm.join guilds.join',
+})
 
 export const user = (({ searchParams }) => {
   // load user from local cache
