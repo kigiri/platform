feb43a647f888b3b0059cadcfdbbe6fbcceea1ce
Clement Denis <le.mikmac@gmail.com>
2021-04-01 03:57:21 +0100

    add discord client oauth

--- index.jsx
+++ index.jsx
@@ -1,26 +1,12 @@
 import { Eve } from './shared/eve.js'
 import { render } from 'react-dom'
+import { Github, Discord } from './icons.jsx'
 import * as React from 'react'
 
-import { authorizationUrl, user } from './oauth.js'
+import { githubUrl, discordUrl, user } from './oauth.js'
 
 // import { FiGithub } from 'react-icons/fi'
 window.React = React
-
-const Github =
-  <svg
-    stroke="currentColor"
-    fill="none"
-    strokeWidth="2"
-    viewBox="0 0 24 24"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    height="2em"
-    width="2em"
-    style={{marignLeft: '2em'}}
-  >
-    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
-  </svg>
 
 render(
   <header>
@@ -38,8 +24,11 @@ render(
     <br />
     <p>
       {user
-        ? <b>Welcome {user.name}</b>
-        : <a href={authorizationUrl}><b>{Github} Login with Github</b></a>
+        ? <>
+            <b>Welcome {user.name}</b>
+            <a href={discordUrl}><b><Discord style={{marignLeft: '2em'}} /> connect discord</b></a>
+          </>
+        : <a href={githubUrl}><b><Github style={{marignLeft: '2em'}} /> Login with Github</b></a>
       }
     </p>
   </header>,
--- oauth.js
+++ oauth.js
@@ -5,20 +5,33 @@ const rand = () => Math.random().toString(36).slice(2, 12).padEnd(10, '0')
 const state = localStorage.state || `${rand()}-${rand()}`
 localStorage.state = state
 
-  // Request a user's GitHub identity
-export const authorizationUrl = `https://github.com/login/oauth/authorize?${[
-  // Required. The client ID you received from GitHub when you registered.
-  `client_id=07957ad810a70e99d67c`,
+// Request a user's GitHub identity
+export const githubUrl = `https://github.com/login/oauth/authorize?${new URLSearchParams(
+  {
+    // Required. The client ID you received from GitHub when you registered.
+    client_id: `07957ad810a70e99d67c`,
 
-  // The URL in your application where users will be sent after authorization.
-  // DEV && `redirect_uri=${encodeURIComponent('http://localhost:8080')}`,
+    // The URL in your application where users will be sent after authorization.
+    // DEV && `redirect_uri=${encodeURIComponent('http://localhost:8080')}`,
 
-  // A random state
-  `state=${state}`,
+    // A random state
+    state,
 
-  // A space-delimited list of scopes.
-  `scope=${encodeURIComponent(['user'].join(' '))}`,
-].filter(Boolean).join('&')}`
+    // A space-delimited list of scopes.
+    scope: 'user',
+  },
+)}`
+
+// Request a user's GitHub identity
+export const discordUrl = `https://discordapp.com/api/oauth2/authorize?${new URLSearchParams(
+  {
+    client_id: '826974634069983282',
+    state,
+    redirect_uri: window.location.origin,
+    response_type: 'code',
+    scope: 'identify gdm.join guilds.join',
+  },
+)}`
 
 export const user = (({ searchParams }) => {
   // load user from local cache
@@ -28,9 +41,10 @@ export const user = (({ searchParams }) => {
   }
 
   // load user from URL
-  const user = Object.fromEntries(searchParams)
+  const { session, ...user } = Object.fromEntries(searchParams)
   localStorage.user = JSON.stringify(user)
   history.replaceState({}, null, '/')
-  document.cookie = `X-User-Id=${user.sid}; path=/; SameSite=Strict`
+  const secure = DEV ? '' : '; secure'
+  document.cookie = `nan-session=${session}; path=/; SameSite=Strict${secure}`
   return user
 })(new URL(window.location))
